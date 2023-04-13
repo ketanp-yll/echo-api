94683580e14dd41ba8e3d8190ff9f7be89cdcfbc
ketanp-yll <ketan@yorklogic.com>
2023-04-13 11:44:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const app = express();
 
-//adding express middleware to serve static pages
 app.use("/api*", function(req, res){
     console.log(req.baseUrl);
 	res.status(200).json(req.headers);
